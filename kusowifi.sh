2b3ce28fad32a284a05ed7d5fe22743f8c2a959b
Sizma yosimaz <57100766+zahmis@users.noreply.github.com>
2021-12-22 08:58:45 +0900

    Update kusowifi.sh

--- kusowifi.sh
+++ kusowifi.sh
@@ -47,5 +47,5 @@ slackData
 
 # 環境変数は crontab に書いている
 # 通常の path では無理っぽい。直にコマンド打つのであればいける
-curl -X POST -H 'Content-type: application/json' --data "$(slackData)" "$(KUSO)"
+curl -X POST -H 'Content-type: application/json' --data "$(slackData)" "$(env.KUSO)"
 
