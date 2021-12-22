9d104eb71a14018b1e4b7f91355004f00df90428
zahmis <meda829ratteru86@gmail.com>
2021-12-23 01:32:53 +0900

    use sh

--- kusowifi.sh
+++ kusowifi.sh
@@ -47,5 +47,5 @@ echo $TEAM
 # 環境変数は crontab に書いている
 # 通常の path では無理っぽい。直にコマンド打つのであればいける
 # ci で直に url を書くと gitgurdian が差し止める。invalid になった後で sh で叩いても invalid
-curl -X POST -H 'Content-type: application/json' --data "$(slackData)" `https://hooks.slack.com/services/$TEAM/$BSTART/$TOKEN`
+curl -X POST -H 'Content-type: application/json' --data "$(slackData)" $SLACK_WEBHOOK_URL
 
