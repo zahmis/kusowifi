82c8e3ce0eea8b3ad307aec9ec4ac30947d8056e
zahmis <meda829ratteru86@gmail.com>
2021-12-22 23:46:27 +0900

    add

--- kusowifi.sh
+++ kusowifi.sh
@@ -47,5 +47,5 @@ echo $KUSO
 # 環境変数は crontab に書いている
 # 通常の path では無理っぽい。直にコマンド打つのであればいける
 # ci で直に url を書くと gitgurdian が差し止める。invalid になった後で sh で叩いても invalid
-curl -X POST -H 'Content-type: application/json' --data "$(slackData)" $KUSO
+curl -X POST -H 'Content-type: application/json' --data "$(slackData)" "$KUSO"
 
